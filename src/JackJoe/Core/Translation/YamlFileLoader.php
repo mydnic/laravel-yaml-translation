f3c4830503b28db6bb42c7919ca419d22f5f51fb
Clément Rigo <rigoclement@mydnic.be>
2023-02-24 11:40:22 +0200

    fix

--- src/JackJoe/Core/Translation/YamlFileLoader.php
+++ src/JackJoe/Core/Translation/YamlFileLoader.php
@@ -1,65 +1,218 @@
 <?php namespace JackJoe\Core\Translation;
 
+use Illuminate\Support\Arr;
+use Symfony\Component\Yaml\Yaml;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Translation\FileLoader;
-use Symfony\Component\Yaml\Yaml;
 
 class YamlFileLoader extends FileLoader {
-  protected function getAllowedFileExtensions() {
-    return ['php', 'yml', 'yaml'];
-  }
+  /**
+     * The filesystem instance.
+     *
+     * @var Filesystem
+     */
+    protected $files;
 
-  protected function loadNamespaceOverrides(array $lines, $locale, $group, $namespace) {
-    foreach ($this->getAllowedFileExtensions() as $extension) {
-      $file = "{$this->path}/packages/{$locale}/{$namespace}/{$group}." . $extension;
+    /**
+     * The default path for the loader.
+     *
+     * @var string
+     */
+    protected $path;
 
-      if ($this->files->exists($file)) {
-        return $this->replaceLines($extension, $lines, $file);
-      }
+    /**
+     * All of the namespace hints.
+     *
+     * @var array
+     */
+    protected $hints = [];
+
+    /**
+     * Create a new file loader instance.
+     *
+     * @param  Filesystem $files
+     * @param  string     $path
+     */
+    public function __construct(Filesystem $files, $path)
+    {
+        $this->path  = $path;
+        $this->files = $files;
     }
 
-    return $lines;
-  }
-
-  protected function replaceLines($format, $lines, $file) {
-    return array_replace_recursive($lines, $this->parseContent($format, $file));
-  }
-
-  protected function parseContent($format, $file) {
-    $content = null;
-
-    switch ($format) {
-    case 'php':
-      $content = $this->files->getRequire($file);
-      break;
-    case 'yml':
-    case 'yaml':
-      $content = $this->parseYamlOrLoadFromCache($file);
-      break;
+    /**
+     * Get Allowed Translation File Extensions.
+     *
+     * @return array
+     */
+    protected function getAllowedFileExtensions()
+    {
+        return ['php', 'yml', 'yaml'];
     }
 
-    return $content;
-  }
+    /**
+     * Load a local namespaced translation group for overrides.
+     *
+     * @param  array  $lines
+     * @param  string $locale
+     * @param  string $group
+     * @param  string $namespace
+     *
+     * @return array
+     */
+    protected function loadNamespaceOverrides(array $lines, $locale, $group, $namespace)
+    {
+        foreach ($this->getAllowedFileExtensions() as $extension) {
+            $file = "{$this->path}/vendor/{$namespace}/{$locale}/{$group}.{$extension}";
 
-  protected function loadPath($path, $locale, $group) {
-    foreach ($this->getAllowedFileExtensions() as $extension) {
-      if ($this->files->exists($full = "{$path}/{$locale}/{$group}." . $extension)) {
-        return $this->parseContent($extension, $full);
-      }
+            if ($this->files->exists($file)) {
+                return array_replace_recursive($lines, $this->parseContent($extension, $file));
+            }
+        }
+
+        return $lines;
     }
 
-    return [];
-  }
+    /**
+     * Load a locale from a given path.
+     *
+     * @param  string $path
+     * @param  string $locale
+     * @param  string $group
+     *
+     * @return array
+     */
+    protected function loadPath($path, $locale, $group)
+    {
+        foreach ($this->getAllowedFileExtensions() as $extension) {
+            if ($this->files->exists($full = "{$path}/{$locale}/{$group}.{$extension}")) {
+                return $this->parseContent($extension, $full);
+            }
+        }
 
-  protected function parseYamlOrLoadFromCache($file) {
-    $cachefile = storage_path() . '/framework/cache/yaml.lang.cache.' . md5($file) . '.php';
-
-    if (@filemtime($cachefile) < filemtime($file)) {
-      $content = Yaml::parseFile($file);
-      file_put_contents($cachefile, "<?php" . PHP_EOL . PHP_EOL . "return " . var_export($content, true) . ";");
-      return $content;
+        return [];
     }
 
-    return $this->files->getRequire($cachefile);
-  }
+    /**
+     * Require regular php file or parse Yaml before loading it
+     *
+     * @param  string $extension
+     * @param  string $file
+     *
+     * @return array
+     */
+    protected function parseContent($extension, $file)
+    {
+        $content = null;
+
+        switch ($extension) {
+            case 'php':
+                $content = $this->files->getRequire($file);
+                break;
+            case 'yml':
+            case 'yaml':
+                $content = $this->parseYamlOrLoadFromCache($file);
+                break;
+        }
+
+        return $content;
+    }
+
+    /**
+     * Parse a Yaml file and return as a php array or load from cache.
+     *
+     * @param  string $file
+     *
+     * @return array
+     */
+    protected function parseYamlOrLoadFromCache($file)
+    {
+        $cachedir  = storage_path() . '/yaml-translation/';
+        $cachefile = $cachedir . 'cache.' . md5($file) . '.php';
+
+        if (@filemtime($cachefile) < filemtime($file)) {
+            $content = null === ($yaml = Yaml::parse(file_get_contents($file))) ? [] : $yaml;
+
+            if (! file_exists($cachedir)) {
+                @mkdir($cachedir, 0755);
+            }
+
+            file_put_contents($cachefile, "<?php" . PHP_EOL . PHP_EOL . "return " . var_export($content, true) . ";");
+        } else {
+            $content = require $cachefile;
+        }
+
+        return $content;
+    }
+
+    /**
+     * Load the messages for the given locale.
+     *
+     * @param  string $locale
+     * @param  string $group
+     * @param  string $namespace
+     *
+     * @return array
+     */
+    public function load($locale, $group, $namespace = null)
+    {
+        if (is_null($namespace) || $namespace == '*') {
+            return $this->loadPath($this->path, $locale, $group);
+        }
+
+        return $this->loadNamespaced($locale, $group, $namespace);
+    }
+
+    /**
+     * Load a namespaced translation group.
+     *
+     * @param  string $locale
+     * @param  string $group
+     * @param  string $namespace
+     *
+     * @return array
+     */
+    protected function loadNamespaced($locale, $group, $namespace)
+    {
+        if (isset($this->hints[$namespace])) {
+            $lines = $this->loadPath($this->hints[$namespace], $locale, $group);
+
+            return $this->loadNamespaceOverrides($lines, $locale, $group, $namespace);
+        }
+
+        return [];
+    }
+
+    /**
+     * Add a new namespace to the loader.
+     *
+     * @param  string $namespace
+     * @param  string $hint
+     *
+     * @return void
+     */
+    public function addNamespace($namespace, $hint)
+    {
+        $this->hints[$namespace] = $hint;
+    }
+
+    /**
+     * Get an array of all the registered namespaces.
+     *
+     * @return array
+     */
+    public function namespaces()
+    {
+        return $this->hints;
+    }
+
+    /**
+     * Add a new JSON path to the loader.
+     *
+     * @param  string  $path
+     * @return void
+     */
+    public function addJsonPath($path)
+    {
+        //
+    }
 }
